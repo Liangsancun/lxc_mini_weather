05ed1b4ea0a04ab3f6a9f2162623cf4070bda2c0
lxc <13205922680@163.com>
2018-10-20 09:11:14 +0800

    get the list of cities from data

---
+++ app/src/main/java/cn/edu/pku/liangxiaochong/app/MyApplication.java
@@ -0,0 +1,107 @@
+package cn.edu.pku.liangxiaochong.app;
+
+import android.app.Application;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.pku.liangxiaochong.bean.City;
+import cn.edu.pku.liangxiaochong.db.CityDB;
+
+public class MyApplication extends Application {
+    private static final String TAG = "MyAPP";
+
+    private static MyApplication myApplication;
+
+    private CityDB myCityDB;
+    private List<City> myCityList;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "MyApplication->Oncreate");
+
+        myApplication = this;
+        myCityDB = openCityDB();
+        initCityList();
+    }
+
+    //初始化城市信息
+    private void initCityList() {
+        myCityList = new ArrayList<City>();
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                //TODO Auto-generated method stub
+                prepareCityList();
+            }
+        }).start();
+    }
+    private boolean prepareCityList() {
+        myCityList = myCityDB.getAllCity();
+        int i = 0;
+        for(City city : myCityList) {
+            i++;
+            String cityName = city.getCity();
+            String cityCode = city.getNumber();
+            Log.d(TAG, cityCode+":"+cityName);
+        }
+        Log.d(TAG, "i="+i);
+        return true;
+    }
+
+    public List<City> getCityList() {
+        return myCityList;
+    }
+    public static MyApplication getInstance() {
+        return myApplication;
+    }
+
+    private CityDB openCityDB() {
+        String path = "/data"
+                + Environment.getDataDirectory().getAbsolutePath()
+                + File.separator + getPackageName()
+                + File.separator + "databases1"
+                + File.separator
+                + CityDB.CITY_DB_NAME;
+        File db = new File(path);
+        Log.d(TAG, path);
+        if(!db.exists()) {
+            String pathfolder = "/data"
+                    + Environment.getDataDirectory().getAbsolutePath()
+                    + File.separator + getPackageName()
+                    + File.separator + "databases1"
+                    + File.separator;
+            File dirFirstFolder = new File(pathfolder);
+
+            if(!dirFirstFolder.exists()) {
+                dirFirstFolder.mkdirs();
+                Log.i("MyApp", "mkdirs");
+            }
+            Log.i("MyApp", "db is not exists");
+            try {
+                InputStream is = getAssets().open("city.db");
+                FileOutputStream fos = new FileOutputStream(db);
+                int len = -1;
+                byte[] buffer = new byte[1024];
+                while((len=is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, len);
+                    fos.flush();
+                }
+                fos.close();
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
+        return new CityDB(this, path);
+    }
+}
---
+++ app/src/main/java/cn/edu/pku/liangxiaochong/bean/City.java
@@ -0,0 +1,36 @@
+package cn.edu.pku.liangxiaochong.bean;
+
+public class City {
+    private String province;
+    private String city;
+    private String number;
+    private String firstPY;
+    private String allPY;
+    private String allFirstPY;
+    //构造函数
+    public City(String province, String city, String number, String firstPY, String allPY, String allFirstPY) {
+        this.province = province;
+        this.city = city;
+        this.number = number;
+        this.firstPY = firstPY;
+        this.allPY = allPY;
+        this.allFirstPY = allFirstPY;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public String getFirstPY() {
+        return firstPY;
+    }
+    public String getAllFirstPY() {
+        return allFirstPY;
+    }
+
+}
